a70eed59e3f8caa8ba57df540d788adf7ccfa595
Element1 <jesus.duran@element.com.mx>
2017-06-20 13:56:41 -0500

    estructura de archivos para trabajar con gulp

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'Tema1WP');
+define('DB_NAME', 'tema1WP');
 
 /** MySQL database username */
-define('DB_USER', 'admin');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'admin');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
